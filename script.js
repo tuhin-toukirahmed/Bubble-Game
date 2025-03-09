7e76c3219be2c390ecd5a3aafcc466489590cbc7
Tuhin-DEK <toukirtuhinfb002@gmail.com>
2025-03-09 17:22:31 +0600

    Creat Bubble Game

---
+++ script.js
@@ -0,0 +1,71 @@
+let time = 10;
+let score = 0;
+let randomNum = Math.floor(Math.random() * 10);
+
+document.getElementById("start").addEventListener("click", () => {
+  document.getElementById("start").style.display = "none";
+  document.getElementById("pbtm").style.flexDirection = "row";
+  makeBubble();
+  timer();
+});
+
+function makeBubble() {
+  for (let i = 1; i < 144; i++) {
+    let x = Math.floor(Math.random() * 10);
+    document.getElementById(
+      "pbtm"
+    ).innerHTML += `<div id="bubbles"> ${x}</div>`;
+  }
+}
+
+function timer() {
+  let timer = setInterval(() => {
+    if (time > 0) {
+      time--;
+      document.getElementById("timer").innerHTML = time;
+    } else {
+      document.getElementById("pbtm").innerHTML = `<h1>Game Over</h1> 
+       <h2>Score: ${score}</h2>
+       <h>Reload to play again</h2>`;
+      clearInterval(timer);
+      document.getElementById("pbtm").style.flexDirection = "column";
+    }
+  }, 1000);
+}
+
+function getHit() {
+  randomNum = Math.floor(Math.random() * 10);
+  document.getElementById("hit").innerHTML = randomNum;
+}
+
+function scoreCount() {
+  score += 1;
+  document.getElementById("score").innerText = score;
+}
+
+document.getElementById("pbtm").addEventListener("click", (dets) => {
+  let hit = Number(document.getElementById("hit").innerText);
+  let bubbles = Number(dets.target.innerText);
+
+  if (hit === bubbles) {
+    scoreCount();
+    getHit();
+    document.getElementById("pbtm").innerHTML = ""; // Clear existing bubbles
+    makeBubble(); // Generate a new set of bubbles
+  }
+});
+
+// document.getElementById("pbtm").addEventListener("click", (dets) => {
+//   let hit = document.getElementById("hit").innerText;
+//   let bubbles = dets.target.innerText;
+//   if (hit === bubbles) {
+//     scoreCount();
+//     getHit();
+//     // dets.target.remove();
+//     // makeBubble();
+//   }
+// });
+
+// timer();
+// makeBubble();
+getHit();
